6eeafd77abd77dc818345b0d18c4793ceecfa622
Coutaq <coutaq@gmail.com>
2022-04-08 23:25:01 +0300

    asdasd

--- resources/js/components/ProductCard.vue
+++ resources/js/components/ProductCard.vue
@@ -9,7 +9,7 @@ console.log(product)
 <template>
    
     <div
-        class="rounded-lg shadow-lg bg-white w-4/5 lg:w-1/5 h-[432px] flex-none hover:shadow-2xl hover:scale-105 duration-300 flex flex-col justify-between"
+        class="rounded-lg shadow-lg bg-white col-span-10 md:col-span-5 lg:col-span-3 h-[432px] flex-none hover:shadow-2xl hover:scale-105 duration-300 flex flex-col justify-between"
     >
             <img
                 class="rounded-t-lg"
@@ -17,14 +17,14 @@ console.log(product)
             
             />
         <div class="px-6">
-            <h5 class="text-gray-600 text-md font-light">{{product.subcategory.category.title}}</h5>
-            <h5 class="text-gray-900 text-xl font-medium mb-2">{{product.title}}</h5>
-            <p class="text-emerald-700 text-xl my-2">
+            <h5 class="text-gray-600 text-sm font-light">{{product.subcategory.category.title}}</h5>
+            <h5 class="text-gray-900 text-md font-medium mb-2">{{product.title}}</h5>
+            <p class="text-emerald-700 text-md my-2">
                 {{product.price}} руб.
             </p>
         </div>
         <div class="flex justify-between items-center px-6 mb-3">
-            <a href="#" class="text-blue-600 text-lg font-bold">Узнать больше</a>
+            <a href="#" class="text-blue-600 text-md font-bold">Узнать больше</a>
             <button
                 type="button"
                 class="inline-block px-6 py-2.5 bg-amber-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-amber-700 hover:shadow-lg focus:bg-amber-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-amber-800 active:shadow-lg transition duration-150 ease-in-out"
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,15 @@
 @extends('layouts.app')
 @inject('products', 'App\Services\ProductService')
 @section('content')
-<div class="flex flex-col flex-wrap lg:flex-row gap-10 justify-center">
-    @foreach ($products->random(8) as $p)
-    <product-card product="{{$p}}"></product-card>
-    @endforeach
+<nav class="col-span-12 p-8 h-32 bg-slate-400"> 
+</nav>
+
+{{-- <div class="flex flex-col flex-wrap lg:flex-row gap-10 justify-center"> --}}
+<div class=" grid grid-cols-12 gap-8 col-span-12 mx-auto max-w-7xl p-8">
+        <h1 class="col-span-12 text-4xl font-normal text-slate-900 my-3">Самые популярные товары</h1>
+        @foreach ($products->random(8) as $p)
+        <product-card product="{{$p}}"></product-card>
+        @endforeach
 </div>
+{{-- </div> --}}
 @endsection
